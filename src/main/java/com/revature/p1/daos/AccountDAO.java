45923e6a3287cdcbef5b7d4035c3655304c69cc9
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2021-05-31 19:13:29 -0400

    added query maker line to post and delete dao methods.  Removed old SQL code from Dao's

--- src/main/java/com/revature/p1/daos/AccountDAO.java
+++ src/main/java/com/revature/p1/daos/AccountDAO.java
@@ -46,25 +46,6 @@ public class AccountDAO {
             acct.setaID(aId);
             balanceDAO.saveNewBalance(new AccountBalance(aId, 0));
 
-//            String sqlInsertAcct = "insert into account " +
-//                    "(user_id , type_id, acct_name) values (?,?,?)";
-//            PreparedStatement pstmt = conn.prepareStatement(sqlInsertAcct, new String[] { "id" });
-//
-//            pstmt.setInt(1,newAcct.getuID());
-//            pstmt.setInt(2, newAcct.gettID());
-//            pstmt.setString(3, newAcct.getaName());
-//
-//            int rowsInserted = pstmt.executeUpdate();
-//
-//            if (rowsInserted != 0) {
-//                ResultSet rs = pstmt.getGeneratedKeys();
-//                while (rs.next()) {
-//                    newAcct.setaID(rs.getInt("id"));
-//                }
-//            }
-//
-//            balanceDAO.saveNewBalance(newAcct);
-
         } catch (SQLException | IllegalAccessException throwables) {
             throwables.printStackTrace();
         }
@@ -96,38 +77,6 @@ public class AccountDAO {
             Account example = new Account();
             Object[] fkInfo = {"user_id", bankUser.getuID()};
             allAccounts = objectMaker.buildObjects(AccountTransaction.class, queryMaker.getObjectByForeignKey(example, fkInfo));
-
-//            String sqlGetNumOfAccts = "select count(*) " +
-//                    "from account where user_id = ?";
-//            PreparedStatement pstmt = conn.prepareStatement(sqlGetNumOfAccts);
-//            pstmt.setInt(1,bankUser.getuID());
-//
-//            ResultSet rs = pstmt.executeQuery();
-//            while (rs.next()) {
-//                count = rs.getInt("count");
-//            }
-//
-//            accts = new Account[count];
-//
-//            String sqlGetAcct = "select * " +
-//                    "from account where user_id = ?";
-//            pstmt = conn.prepareStatement(sqlGetAcct);
-//
-//            pstmt.setInt(1,bankUser.getuID());
-//
-//            rs = pstmt.executeQuery();
-//            while (rs.next()) {
-//                acct = new Account();
-//
-//                acct.setaID(rs.getInt("id"));
-//                acct.setaName(rs.getString("acct_name"));
-//                acct.setuID(rs.getInt("user_id"));
-//                acct.setjUID(rs.getInt("joint_user_id"));
-//                acct.settID(rs.getInt("type_id"));
-//
-//                accts[rsCounter] = acct;
-//                rsCounter++;
-//            }
 
         } catch (SQLException | IllegalAccessException | NoSuchMethodException | InvocationTargetException | InstantiationException throwables) {
             throwables.printStackTrace();
